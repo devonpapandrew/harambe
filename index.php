1b042301ac59a2196ae0949e814ee5a89152f3de
Devon Papandrew <devon.papandrew@gmail.com>
2016-12-21 23:50:48 -0500

    name

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if($inputMsg['sender_id'] == 362006){ //Harambe bot sender id
     exit;
 }
 
-send($inputMsg['text']);
+send($inputMsg['name']);
 
 
 if(strpos($inputMsg['name'], 'Marcus') || strpos($inputMsg['name'], 'marcus')){
