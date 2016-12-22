154359b2949f063af4f8a5567c8695aea0d05df6
Devon Papandrew <devonpapandrew@users.noreply.github.com>
2016-12-21 23:45:59 -0500

    exit upon harambe sending msg

--- index.php
+++ index.php
@@ -1,24 +1,52 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-$data = json_decode(file_get_contents('php://input'), true);
+//grab the POSTed input msg
+$inputMsg = json_decode(file_get_contents('php://input'), true);
 
-send($data['sender_id']);
+/* Looks like:
 
-if(strpos($_POST['name'], 'Marcus') || strpos($_POST['name'], 'marcus')){
+{
+  "attachments": [],
+  "avatar_url": "http://i.groupme.com/123456789",
+  "created_at": 1302623328,
+  "group_id": "1234567890",
+  "id": "1234567890",
+  "name": "John",
+  "sender_id": "12345",
+  "sender_type": "user",
+  "source_guid": "GUID",
+  "system": false,
+  "text": "Hello world ☃☃",
+  "user_id": "1234567890"
+}
+
+*/
+
+//Kill the process if the last msg sent was from this bot
+if($inputMsg['sender_id'] == 362006){ //Harambe bot sender id
+    exit;
+}
+
+if(strpos($inputMsg['name'], 'Marcus') || strpos($inputMsg['name'], 'marcus')){
     send('SHUT UP MARCUS');
 }
 
-if(strpos($_POST['text'], 'moon') || strpos($_POST['text'], 'lunar')){
+if(strpos($inputMsg['text'], 'moon') || strpos($inputMsg['text'], 'lunar')){
     $text = "That's one small step for man, one giant leap for mankind.";
     send($text);
 }
 
-if(strpos($_POST['text'], 'lauren') || strpos($_POST['text'], 'Lauren')){
+if(strpos($inputMsg['text'], 'lauren') || strpos($_POST['text'], 'Lauren')){
     $text = "smh lauren lol";
     send($text);
 }
 
+
+/**
+ * Sends the $text to the group as Harambe
+ * @param $text
+ */
 function send($text){
     // where are we posting to?
     $url = 'https://api.groupme.com/v3/bots/post';
